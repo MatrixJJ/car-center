369028f4189633219232f7045bb7718b904cce63
sj <shengjie0629@gmail.com>
2021-06-19 01:51:11 +0800

    Initial commit

---
+++ car-center-show/src/main/java/com/sj/car/show/CarCenterShowApplication.java
@@ -0,0 +1,13 @@
+package com.sj.car.show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCenterShowApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CarCenterShowApplication.class, args);
+  }
+
+}
